511e96e73424980853ff33654a20707acc2ba7c7
Larissa Cristina Benedito <mewmewdevart@gmail.com>
2021-12-26 15:02:30 -0300

    putting old files in the repository

---
+++ modelocard.js
@@ -0,0 +1,4 @@
+$(".sexytabs").tabs({ 
+  show: { effect: "slide", direction: "left", duration: 200, easing: "easeOutBack" } ,
+  hide: { effect: "slide", direction: "right", duration: 200, easing: "easeInQuad" } 
+});
